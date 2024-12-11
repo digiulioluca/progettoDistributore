834916fdb60e22b4138011c24dba4ce3cc737747
Alessandro Marasca <linkmarasca@gmail.com>
2024-12-11 16:16:47 +0100

    Secondo commit, modifiche admin

--- ProgettoDistributore/src/distributore/Admin.java
+++ ProgettoDistributore/src/distributore/Admin.java
@@ -1,5 +1,31 @@
 package distributore;
-
+import java.util.ArrayList;
+/*
+ * attributi di admin? codice, transazione,
+ */
 public class Admin {
+	int codice;
+	int transazione;
+	int importo;
+	String prodotto;
+	
+	//ARRAYLIST PER CONTENERE I PRODOTTI
+	ArrayList <Prodotto> prodotti = new ArrayList<Prodotto>();
+	
+	public void aggiungi (Prodotto prod) {
+		for(Prodotto agg: prodotti) {
+			if(agg. ) {
+				break;
+			}
+		}
+ 
 
+		
+	}
+	public void selezionato(int importo, int codice) {
+		
+	}
+	public void elencoop(int transazioni) {
+		
+	}
 }
--- ProgettoDistributore/src/distributore/Alcolici.java
+++ ProgettoDistributore/src/distributore/Alcolici.java
@@ -1,5 +1,5 @@
 package distributore;
 
 public class Alcolici {
-
+	
 }
