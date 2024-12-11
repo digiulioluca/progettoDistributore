390743a04cee909bc2fa0a5096cf4a91fb92d68d
Luca Di Giulio <lkdgl95@gmail.com>
2024-12-11 15:33:53 +0100

    Commit. Modifica classi figlie

--- ProgettoDistributore/src/distributore/Caffetteria.java
+++ ProgettoDistributore/src/distributore/Caffetteria.java
@@ -1,8 +1,8 @@
 package distributore;
 
-import java.util.Scanner;
 
 public class Caffetteria extends Prodotto {
+	// attributi
 	protected int qtaZucchero;
 	protected int qtaCucchiaini;
 	
@@ -10,10 +10,22 @@ public class Caffetteria extends Prodotto {
 		super(nome, prezzo, qta);
 	}
 	
-	public void erogaZucchero(int scelta, Scanner scanner) {
-		
-		for (int i = 1; i<scelta; i++) {
-			qtaZucchero -= i;
+	// metodo per erogazione dello zucchero
+	public void erogaZucchero(int scelta) {
+		/* controllo sulla quantità scelta dall'utente con la quantità presente
+		 in magazzino di zucchero*/
+		if (scelta<qtaZucchero) {
+			for (int i = 1; i<scelta; i++) {
+					qtaZucchero -= i;
+			}
+		}else{
+			System.out.println("Impossibile erogare perché hai inserito un numero"
+						+"maggiore rispetto alla quantità di zucchero.");	
 		}
+	}
+	
+	// metodo per erogare cucchiaini, simile al precedente
+	public void erogaCucchiaini() {
+		qtaCucchiaini--;
 	}
 }
