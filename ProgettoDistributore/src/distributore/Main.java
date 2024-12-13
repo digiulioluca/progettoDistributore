be5aef49eed3455f1d24271bdbe288891e998b3d
FiammaTri <scinty-pcd@live.it>
2024-12-13 14:55:14 +0100

    novità

--- ProgettoDistributore/src/distributore/Main.java
+++ ProgettoDistributore/src/distributore/Main.java
@@ -22,7 +22,7 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		//System.out.println("Hello da Fiamma");
+		
 
 		int scelta;
 		Scanner scanner = new Scanner(System.in);
